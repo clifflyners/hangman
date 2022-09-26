b3014fe2fa9ddbcf2f68d647eb960ee98b294ab8
Zembik92 <kambouazembi@gmail.com>
2022-09-26 16:28:41 +0100

    first milestone commit

---
+++ hangman/milestone_1.py
@@ -0,0 +1,19 @@
+#import a python module
+import random
+#Create a list of your favorite fruits
+word_list = ["apple", "banana", "pineapple", "mango", "grapes"]
+print(word_list)
+#Using the random module, randomly print one of your favorite fruit
+word = random.choice(word_list)
+print(word)
+#Using the input function, ask the user to input a letter
+guess = input("Enter a single letter: ")
+# Ensure the input is one letter
+if len(guess) == 1 and guess.isalpha():
+        print("Good guess")
+else:
+        print("Oops! that is not a valid input")
+            
+
+
+
